42b787dc6b7f560e045c6cc9e271bea7f6c0af7e
cloudYa <monniyastyle@gmail.com>
2014-12-15 23:19:35 +0800

    category-moment v1

--- wp-content/themes/fairy/slider.php
+++ wp-content/themes/fairy/slider.php
@@ -4,19 +4,19 @@
               <li>
                 <img src="http://ft.dlinkddns.com/wordpress/wp-content/themes/fairy/images/content/slide_01.jpg" alt="" />
                 <div class="flex-caption">
-                    <h3>泸沽湖畔</h3> <p>我看不清远方，但一直在眺望。</p> <input type="submit" value="更多" class="red" />
+		<h3>泸沽湖畔</h3> <p>我看不清远方，但一直在眺望。</p> <a href="<?php get_bloginfo('template_directory');?>?cat=4 "><input type="submit" value="更多" class="red" /></a>
                 </div>
               </li>
               <li>
                 <img src="http://ft.dlinkddns.com/wordpress/wp-content/themes/fairy/images/content/slide_02.jpg" alt="" />
                 <div class="flex-caption">
-                    <h3>女神湾</h3> <p>那一滴泪，落在我心，荡漾出一湖的悲伤。</p> <input type="submit" value="更多" class="red" />
+		<h3>女神湾</h3> <p>那一滴泪，落在我心，荡漾出一湖的悲伤。</p><a href="<?php get_bloginfo('template_directory');?>?cat=6 "> <input type="submit" value="更多" class="red" /></a>
                 </div>
               </li>
               <li>
                 <img src="http://ft.dlinkddns.com/wordpress/wp-content/themes/fairy/images/content/slide_03.jpg" alt="" />
                 <div class="flex-caption">
-                    <h3>日出</h3> <p>那一瞬的出现，黑暗淡去，光明降临。</p> <input type="submit" value="更多" class="red" />
+		<h3>日出</h3> <p>那一瞬的出现，黑暗淡去，光明降临。</p> <a href="<?php get_bloginfo('template_directory');?>?cat=3 "><input type="submit" value="更多" class="red" /></a>
                 </div>
               </li>
             </ul>
